43b663aea09a9a9a0485d36cfbad6e08514c91f2
Miraj Mainali <merazzmainali05@gmail.com>
2024-05-18 23:13:04 +1000

    completed the needed front-end for api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
-import MovieCard from "@/components/MovieCardComponent/MovieCard";
+"use client";
+
 import SearchComponent from "@/components/SearchComponent/SearchComponent";
 
 export default function Home() {
+  const searchParam = (searchWords: string): string => searchWords;
+
   return (
-    <div className="flex  flex-col items-center">
+    <div className="relative  flex flex-col items-center">
       <SearchComponent inputPlaceholder={"Search your movie here..."} />
-      <div className="text-blue-300 mt-16 text-2xl">Popular Movies</div>
-      <div className="my-6 mt-6 grid w-10/12 grid-cols-2 gap-y-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </div>
+
+      <div className="mt-16 text-2xl text-blue-300">Popular Movies</div>
+      <div className="my-6 mt-6 grid w-10/12 grid-cols-2 gap-y-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"></div>
     </div>
   );
 }
--- src/components/SearchComponent/SearchComponent.tsx
+++ src/components/SearchComponent/SearchComponent.tsx
@@ -1,31 +1,32 @@
-import React from "react";
-import Button from "../Buttons/Buttons";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 
 interface SearchComponentProps {
-  buttonName?: string;
   inputPlaceholder: string;
 }
 
-const SearchComponent = ({
-  inputPlaceholder,
-}: SearchComponentProps) => {
+const SearchComponent = ({ inputPlaceholder }: SearchComponentProps) => {
+  const [searchText, setSearchText] = useState("");
+
+  const here = () => {
+    alert(searchText);
+  };
+
   return (
     <div className="mt-6 flex w-11/12 items-center justify-between rounded-full border border-blue-300 text-blue-300 focus:border-blue-600 sm:w-10/12 lg:w-8/12">
       <input
         type="search"
         placeholder={inputPlaceholder}
-        className="  w-8/12 flex-grow rounded-full bg-transparent p-4 text-center placeholder-blue-200 caret-blue-400 outline-none transition-all duration-75 focus:scale-105"
+        className="  w-6/12 flex-grow rounded-full bg-transparent p-4 text-center placeholder-blue-200 caret-blue-400 outline-none transition-all duration-75 focus:scale-105"
+        onChange={(e) => setSearchText(e.target.value)}
       />
 
       <div className="h-14 grow-0 border-l border-blue-300"></div>
 
       <div className="flex grow-0 items-center justify-center p-0">
-        <Button
-          bgColor="bg-transparent"
-          icon={CiSearch}
-          textColor="text-blue-300"
-        />
+        <button className="bt-transparent flex w-32 cursor-pointer items-center justify-center gap-1 p-1 text-2xl transition-all hover:scale-125">
+          <CiSearch />
+        </button>
       </div>
     </div>
   );
